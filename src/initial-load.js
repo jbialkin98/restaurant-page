379a007c3c8bd733ed0d06714b9a2b795b825d9e
Josh Bialkin <jbialkin98@gmail.com>
2022-12-01 18:26:57 -0500

    Make all page content Javascript

--- src/initial-load.js
+++ src/initial-load.js
@@ -1,8 +1,18 @@
 export default function createHomePageElements() {
     const homeContent = document.getElementById('content');
+
     const header = document.createElement('h1');
     header.textContent = "World's Best Restaurant";
     homeContent.appendChild(header);
-    console.log(header);
+
+    const image = document.createElement('img');
+    image.src = "../src/restaurant-image.jpeg";
+    image.style = "width: 50%; height: auto";
+    homeContent.appendChild(image);
+
+    const homeText = document.createElement('p');
+    homeText.textContent = "This restaurant is really great. I eat here three meals a day.";
+    homeContent.appendChild(homeText);
+
     return homeContent;
 }
