59621b777bf4d05298e1eb6b1e5d99fc542edb6c
Josh Bialkin <jbialkin98@gmail.com>
2022-12-02 13:58:59 -0500

    Add buttons and clear page on click

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import loadContent from './initial-load.js';
+import './style.css';
 
 loadContent();
+
+const content = document.getElementById('content');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(btn => btn.addEventListener('click', () => {
+    while (content.firstElementChild) {
+        content.firstElementChild.remove();
+    }
+}));
